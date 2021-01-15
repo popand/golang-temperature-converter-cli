8775a930a93ee9ea10b47b3d9423e117892b25c9
Andrei <anpop@cisco.com>
2021-01-14 20:58:35 -0500

    fixing validation

--- main.go
+++ main.go
@@ -19,11 +19,11 @@ var errReadingInput = errors.New("Error reading input")
 
 func main() {
 
-	if len(os.Args) > 2 {
+	if len(os.Args) != 2 {
 		printError(errInvalidArguments)
 	}
 
-	originUnit := strings.ToUpper(os.Args[1])
+	originUnit = strings.ToUpper(os.Args[1])
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
